d044fa26b992ab116d74035339dcc4bc1bb65472
WuiiiGithub <142606875+WuiiiGithub@users.noreply.github.com>
2026-08-05 12:12:18 +0530

    Updated cloud start

--- cloud_setup.py
+++ cloud_setup.py
@@ -1,14 +1,30 @@
 import os
-print("ENTER '1' IF THE REPO SHOULD BE CLONED")
-print("ENTER '2' IF THE REPO SHOULD BE PULLED")
-inp = input()
-if inp == '1':
-    os.system('git clone https://github.com/WuiiiGithub/Candlicious.git')
-    os.system('mv ./Candilicious/* ./')
-    os.system('mv ./Candilicious/.git ./')
-    os.system('rm -rf ./Candilicious')
+while True:
+    inp = input()
+    print("ENTER 'clone' IF THE REPO SHOULD BE CLONED")
+    print("ENTER 'fetch' IF THE REPO SHOULD BE FETCHED")
 
-elif inp == '2':
-    os.system('git pull https://github.com/WuiiiGithub/Candlicious.git')
+    if inp == 'clone':
+        os.system('git clone https://github.com/WuiiiGithub/Candlicious.git')
+        os.system('mv ./Candilicious/* ./')
+        os.system('mv ./Candilicious/.git ./')
+        os.system('rm -rf ./Candilicious')
+
+    elif inp == 'fetch':
+        os.system('git remote add origin https://github.com/WuiiiGithub/Candlicious.git')
+        os.system('git fetch origin v1.x')
+
+    elif inp == 'start':
+        os.system("/home/container/.venv/bin/python3 main.py")
+        break
+
+    elif inp == 'install': 
+        os.system("/home/container/.venv/bin/pip requirements.txt")
+
+    elif inp == 'venv':
+        os.system('python3 -m venv .venv')
+
+    else:
+        os.system(inp)
 
 print('='*1000)
